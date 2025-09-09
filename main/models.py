c71e285d53b3c02edafaa9391ecac3e641864bfa
Hafiz Nathan <hafiz.nathan@ui.ac.id>
2025-09-09 21:52:47 +0700

    Selesai Tugas 2

--- main/models.py
+++ main/models.py
@@ -1,3 +1,34 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('jersey', 'Jersey'),
+        ('shoes', 'Shoes'),
+        ('pants', 'Pants'),
+        ('jacket', 'Jacket'),
+        ('bag', 'Sports Bag'),
+        ('accessory', 'Accessory'),
+    ]
+
+    SIZE_CHOICES = [
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+        ('XL', 'Extra Large'),
+        ('N/A', 'Not Applicable'),  # untuk barang tanpa size
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    size = models.CharField(max_length=3, choices=SIZE_CHOICES, default='N/A')
+    rating = models.FloatField(default=0.0)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='jersey')
+    thumbnail = models.URLField(blank=True, null=True)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
 # Create your views here.
+def show_main(request):
+    context = {
+        'app_name' : 'Humming Crew',
+        'npm' : '2406432406',
+        'name': 'Hafiz Nathan Vesaputra',
+        'class': 'PBP F',
+    }
+
+    return render(request, "main.html", context)
